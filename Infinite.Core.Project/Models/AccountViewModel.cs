a484d712de3c0c4cefe8b54e709173732f6dd809
ArunKumar <arunkumar.e@outlook.com>
2023-01-27 15:44:01 +0530

    Create Register View

---
+++ Infinite.Core.Project/Models/AccountViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Infinite.Core.Project.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and Confirm Password doesn't match")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Display(Name = "Email Id")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string FullName { get; set; }
+
+        // [Required]
+        public string Role { get; set; } = "Customer";
+
+    }
+
+    public class LoginViewModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
